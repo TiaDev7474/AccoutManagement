204ae1b5055bc66ed254ffb1589aed268c6caa81
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-08-25 22:27:20 +0300

    signup page terminer

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import { Avatar} from '@mui/material'
+import React,{ChangeEvent, useState} from 'react'
+import PhotoCameraOutlinedIcon from '@mui/icons-material/PhotoCameraOutlined';
 
 function Home() {
+
   return (
-    <div>
-        <h1>Home</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio recusandae, pariatur officia accusantium vitae libero fuga laboriosam soluta perferendis praesentium a perspiciatis sit, cum aliquam nemo quia magni quas minus.</p>
+    <div className="b">
+    <div className="profile-container">
+        <div className="cover-photo">
+            <img src="cover_photo.jpg" alt="Cover Photo"/>
+        </div>
+        <div className="profile">
+            <img className="profile-picture" src="user_image.jpg" alt="User Image"/>
+            <div className="user-details">
+                <h1 className="user-name">John Doe</h1>
+                <p className="user-email">johndoe@example.com</p>
+                <button className="edit-button">Edit Profile</button>
+            </div>
+        </div>
+        <div className="user-description">
+            <h2>About Me</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ultricies lectus.</p>
+        </div>
+    </div>
     </div>
   )
 }
